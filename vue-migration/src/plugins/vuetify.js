8f3208817069e6c33adb509dda34cd22aa63d9a0
Zack <zacklai.0711@gmail.com>
2023-11-07 10:47:27 +0900

    Extract vuetify settings.

---
+++ vue-migration/src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi' // This is already the default value - only for display purposes
+  }
+})
